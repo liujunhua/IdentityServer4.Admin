d988647cbee07f86c2f266cda5d2060b9e54df72
刘君华 <msnetboy@outlook.com>
2019-03-22 14:15:27 +0800

    IdentityServer4.Api项目提交

---
+++ src/IdentityServer4.Api/Controllers/RoleController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Skoruba.IdentityServer4.Admin.BusinessLogic.Identity.Dtos.Identity;
+using Skoruba.IdentityServer4.Admin.BusinessLogic.Identity.Services.Interfaces;
+using Skoruba.IdentityServer4.Admin.EntityFramework.DbContexts;
+using Skoruba.IdentityServer4.Admin.EntityFramework.Identity.Entities.Identity;
+
+namespace IdentityServer4.Api.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [ApiController]
+    [Route("api/identityadmin/[controller]/[action]")]
+    public class RoleController : Controller
+    {
+
+        private readonly IIdentityService<AdminDbContext, UserDto<int>, int, RoleDto<int>, int, int, int, UserIdentity, UserIdentityRole, int, UserIdentityUserClaim, UserIdentityUserRole, UserIdentityUserLogin, UserIdentityRoleClaim, UserIdentityUserToken> _identityService;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="identityService"></param>
+        public RoleController(IIdentityService<AdminDbContext, UserDto<int>, int, RoleDto<int>, int, int, int, UserIdentity, UserIdentityRole, int, UserIdentityUserClaim, UserIdentityUserRole, UserIdentityUserLogin, UserIdentityRoleClaim, UserIdentityUserToken> identityService)
+        {
+            _identityService = identityService;
+        }
+
+    }
+}
